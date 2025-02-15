39d87a19c627e503b536eaeb3fea27a0654ccb15
rloureiro <ramon.loureiro@upf.edu>
2025-02-15 13:06:26 +0000

    doc docker

--- lib/influxdb_utils.py
+++ lib/influxdb_utils.py
@@ -3,11 +3,16 @@
 import os
 from influxdb_client import InfluxDBClient
 
-# Cargar configuración desde variables de entorno
-INFLUXDB_URL = os.getenv("INFLUXDB_URL", "http://192.168.1.191:8086")
-INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "influxdb-j3Q!zK!RyjHWvMt-wvYq0UbRdRVqYAYPiTBDZkUNCYiyyU0D0dFXcdJ")
-INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "home_assistant")
-INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "ha_data")
+from dotenv import load_dotenv
+
+# Cargar variables desde .env
+load_dotenv()
+
+INFLUXDB_URL = os.getenv("INFLUXDB_URL")
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
+INFLUXDB_ORG = os.getenv("INFLUXDB_ORG")
+INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET")
+
 
 def get_last_temperatures(n=10):
     """Consulta los últimos n valores de temperatura en InfluxDB."""
